630d3b4f13526f928f258743f7a95bba63d0a56e
Trey Chumbley <trey.chumbley@my.simpson.edu>
2019-02-04 19:13:36 -0600

    Assignment 3 work

---
+++ src/edu/simpson/cis320/NameListGet.java
@@ -0,0 +1,35 @@
+package edu.simpson.cis320;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import com.google.gson.Gson;
+
+@WebServlet(name = "NameListGet")
+public class NameListGet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Get setup up to output JSON text
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        // Use our DAO to get a list of people
+        List <Person> peopleList = PersonDAO.getPeople();
+
+        // Create an instance of the Google class that can convert
+        // business object to JSON strings.
+        Gson gson = new Gson();
+
+        // Go ahead and turn peopleList into a JSON string
+        String json = gson.toJson(peopleList);
+
+        // Write out that string
+        out.println(json);
+
+    }
+}
---
+++ src/edu/simpson/cis320/Person.java
@@ -0,0 +1,29 @@
+package edu.simpson.cis320;
+
+public class Person {
+
+    private int id;
+    private String first;
+    private String last;
+    private String phone;
+    private String email;
+    private String birthday;
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirst() {return first; }
+    public void setFirst(String first) {this.first = first; }
+
+    public String getLast() {return first; }
+    public void setLast(String last) {this.last = last; }
+
+    public String getPhone() {return phone; }
+    public void setPhone(String phone) {this.phone = phone; }
+
+    public String getEmail() {return email; }
+    public void setEmail(String email) {this.email = email; }
+
+    public String getBirthday() {return birthday; }
+    public void setBirthday(String birthday) {this.birthday = birthday; }
+}
