8d714d8b8aae0a101064d7bd11a46325399d3209
Trey Chumbley <trey.chumbley@my.simpson.edu>
2019-02-14 09:30:24 -0600

    Assignment 5 work

--- out/artifacts/CIS_320_war_exploded/js/name_list2.js
+++ out/artifacts/CIS_320_war_exploded/js/name_list2.js
@@ -11,10 +11,11 @@ function updateTable() {
             for (var i = 0; i < json_result.length; i++) {
                 //console.log(json_result[i].id + " " + json_result[i].first + " " + json_result[i].last + " "
                    // + json_result[i].email + " " + json_result[i].phone + " " + json_result[i].birthday);
-
+                var phoneNum = json_result[i].phone;
+                var phone = phoneNum.substring(0,3) + "-" + phoneNum.substring(3,6) + "-" + phoneNum.substring(6,10);
                 $('#datatable tr:last').after('<tr><td>' + json_result[i].id + '</td><td>' + json_result[i].first +
                     '</td><td>' + json_result[i].last + '</td><td>' + json_result[i].email + '</td><td>' +
-                    json_result[i].phone + '</td><td>' + json_result[i].birthday + '</td></tr>')
+                    phone + '</td><td>' + json_result[i].birthday + '</td></tr>')
 
 
             }
@@ -23,5 +24,36 @@ function updateTable() {
     );
 }
 
+//called when "Add Item" button is clicked
+function showDialogAdd() {
+
+    //print that we got here
+    console.log("Opening add item dialog");
+
+    //Clear out the values in the form.
+    //Otherwise we'll keep values from when we last opened or hit edit.
+    //I'm getting started, you  can finish.
+    $('#id').val("");
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#email').val("");
+    $('#phone').val("");
+    $('#birthday').val("");
+
+    //show the hidden dialog
+    $('#myModal').modal('show');
+}
+
+function savedChanges(){
+    console.log("Attempting to save changes")
+    $('#myModal').modal('hide');
+}
+
 //call code
 updateTable();
+
+var addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
+
+var saveButton = $('#saveChanges');
+saveButton.on("click", savedChanges);
